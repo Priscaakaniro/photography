681c78b22a88a7d49d3b60d718b6d60cd28eb27f
Prisca Akaniro <priscaakaniro@gmail.com>
2023-05-08 13:58:41 +0100

    Files for the photography webiste

---
+++ web.js
@@ -0,0 +1,21 @@
+// When the user scrolls down 50px from the top of the document, resize the header's font size
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    document.getElementById("header").style.fontSize = "15px";
+  } else {
+    document.getElementById("header").style.fontSize = "40px";
+  }
+}
+
+//styling the menu
+function openNav() {
+  document.getElementById("myNav").style.height = "100%";
+}
+
+/* Close */
+function closeNav() {
+  document.getElementById("myNav").style.height = "0%";
+}
+
